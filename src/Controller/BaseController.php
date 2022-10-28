8800713ad5672adcc6b965ab64b6c245e561aeea
alexis mouchon <alexismouchon09@gmail.com>
2022-10-28 15:04:54 +0200

    add documentation

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -23,6 +23,7 @@ class BaseController extends AbstractController
                 "get one user" => "/api/user/{id} --> GET",
                 "update user" => "/api/user/{id}?_method=PUT --> POST",
                 "delete user" => "/api/user/{id} --> DELETE",
+                "paginate user" => "/api/paginate/user?page={int}&limit={int} --> GET",
             ],
             "type" => [
                 "get all types" => "/api/type --> GET",
@@ -30,6 +31,14 @@ class BaseController extends AbstractController
                 "create type" => "/api/type --> POST",
                 "update type" => "/api/type/{id}?_method=PUT --> POST",
                 "delete type" => "/api/type/{id} --> DELETE",
+            ],
+            "trash" => [
+                "get all trashes" => "/api/trash --> GET",
+                "get one trash" => "/api/trash/{id} --> GET",
+                "create trash" => "/api/trash --> POST",
+                "update trash" => "/api/trash/{id}?_method=PUT --> POST",
+                "delete trash" => "/api/trash/{id} --> DELETE",
+                "paginate trash" => "/api/paginate/trash?page={int}&limit={int} --> GET",
             ],
         ]);
     }
